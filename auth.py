f83a90e37ebe73a3261a331daf0d1a516cdc85d7
Eva Maciejko <emaciejko@gmail.com>
2024-05-28 11:38:16 -0400

    Optimize token retrieval by using file persistence

--- auth.py
+++ auth.py
@@ -15,8 +15,10 @@
 
 # [START auth_cloud_idtoken_service_account]
 
+from flask import json
 import google.auth
 import google.auth.transport.requests
+from datetime import datetime
 
 from google.oauth2 import service_account
 
@@ -25,18 +27,17 @@ class ServiceAccountAuth:
     def __init__(self, json_credential_path: str, scope: str):
         self.json_credential_path = json_credential_path
         self.scope = scope        
-        self.token_data = self.get_token_from_serviceaccount()
+        self.token_data = self.__get_token()
 
-    def needs_update(self):
-          return (ServiceAccountAuth.token_data is None or
-                  ServiceAccountAuth.token_data["expiry"] < google.auth._helpers.utcnow() or
-                  ServiceAccountAuth.token_data["json_credential_path"] != self.json_credential_path or
-                  ServiceAccountAuth.token_data["scope"] != self.scope)
+    def __needs_update(self, token_data):
+          return (token_data is None or
+                  datetime.strptime(token_data["expiry"], '%a, %d %b %Y %H:%M:%S GMT') < google.auth._helpers.utcnow() or
+                  token_data["json_credential_path"] != self.json_credential_path or
+                  token_data["scope"] != self.scope)
 
-    def __get_token_from_serviceaccount(self):        
-      """
-      TODO(Developer): Replace the below variables before running the code.
-
+    def __get_token(self):        
+      """      
+      Obtains an ID token using a service account json credential file.
       *NOTE*:
       Using service account keys introduces risk; they are long-lived, and can be used by anyone
       that obtains the key. Proper rotation and storage reduce this risk but do not eliminate it.
@@ -44,22 +45,33 @@ class ServiceAccountAuth:
       does not use a service account key. Several alternatives to service account keys
       are described here:
       https://cloud.google.com/docs/authentication/external/set-up-adc
-
-      Args:
-          json_credential_path: Path to the service account json credential file.
-          target_audience: The url or target audience to obtain the ID token for.
-                          Examples: http://www.abc.com
+            
       """
+      token_data = None
       # Check if the token is already available and not expired.
-      if(self.needs_update()):          
+      try:
+        with open("token.json", "r") as file:
+          token_data = json.load(file)
+        if self.__needs_update(token_data):
+          token_data = self.new_token()
+      
+      except FileNotFoundError:
         # Obtain the id token by providing the json file path and target audience.
+        token_data = self.new_token()        
+      
+      return token_data
+
+    def new_token(self):
         credentials = service_account.Credentials.from_service_account_file(self.json_credential_path)
         scoped_credentials = credentials.with_scopes([self.scope])
         request = google.auth.transport.requests.Request()
         scoped_credentials.refresh(request)
-        return {"token": scoped_credentials.token, "expiry": scoped_credentials.expiry, "scope": self.scope, "json_credential_path": self.json_credential_path}
-      else:
-        return ServiceAccountAuth.token_data
+        token_data = {"token": scoped_credentials.token, "expiry": scoped_credentials.expiry, "scope": self.scope, "json_credential_path": self.json_credential_path}
+        
+        with open("token.json", "w") as file:
+          json.dump(token_data, file)
+
+        return token_data
 
       
     
